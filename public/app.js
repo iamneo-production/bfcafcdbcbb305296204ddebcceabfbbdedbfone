15e02a7b905cfc78074ba221cecbc881ff2935be
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:38:03 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,6 +1,11 @@
-function student_total_marks(){
+function per_student_total_marks(){
     const marks = document.getElementsByClassName("student4").value;
-    const total_mark_per_Student = sum(marks)
+    console.log(marks)
+    const sum = 0
+    marks.array.forEach(element => {
+        sum+=element;
+    });
+    const total_mark_per_Student = sum
     console.log(total_mark_per_Student)
     document.getElementById("result1").innerHTML = sum(total_mark_per_Student)
 }
