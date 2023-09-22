6e2517ed5de48a2b7257eb918c47f4f5ac7c2dc1
Iamneo <techadmin@iamneo.ai>
2023-09-22 08:33:08 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 function per_student_total_marks(){
-    const student_total_marks = document.querySelectorAll(".result")
+    let student_total_marks = document.querySelectorAll(".result")
     console.log(student_total_marks)
-    const total_marks=0
+    let total_marks=0
     
     for(let i=0;i<student_total_marks.length;i++){
-        console.log(student_total_marks[i].nodeValue)
-        total_marks =total_marks+ student_total_marks[i].nodeValue
+
+        total_marks += parseFloat(student_total_marks[i].innerHTML)
     }
-    console.log(total_marks)
+    window.alert(total_marks)
 }
